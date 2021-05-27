57483be0bf28bb318189b9c1bfe0242c693f9483
Miriam Baranovska <maryam1649@gmail.com>
2021-05-27 16:31:29 +0300

    added basic header, still unfinished

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-export class AppHeader extends Component {
-    render() {
-        return (
-            <div>
-                AppHeader
-            </div>
-        )
-    }
+export function AppHeader (props) {
+    return (
+        <header>
+            <nav>
+                <Link to="/">Home</Link>
+                <Link to="/board"> Boards </Link>
+            </nav>
+
+            <nav>
+                {!props.loggedInUser && <Link to="/login"> Login</Link>}
+                {props.loggedInUser && <Link to="/login" style={{ backgroundImage: `url(${props.loggedInUser.imgUrl})` }}> </Link>}
+            </nav>
+        </header>
+    )
 }
