278423552720f46b0523e18af1a7d1bbcf65083f
Miriam Baranovska <maryam1649@gmail.com>
2021-05-27 18:54:26 +0300

    added app header basic

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom'
 
-export function AppHeader (props) {
+export function AppHeader(props) {
     return (
         <header>
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/board"> Boards </Link>
             </nav>
-
+            <nav>app-logo</nav>
             <nav>
                 {!props.loggedInUser && <Link to="/login"> Login</Link>}
                 {props.loggedInUser && <Link to="/login" style={{ backgroundImage: `url(${props.loggedInUser.imgUrl})` }}> </Link>}
--- src/cmps/BoardNav.jsx
+++ src/cmps/BoardNav.jsx
@@ -1,13 +1,57 @@
-import { NavLink } from 'react-router-dom'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export function BoardNav({  isMobileNavOpen, onToggleMobileNav }) {
 
-    return (
-        <nav className={`main-nav ${isMobileNavOpen ? 'open' : ''}`}>
-            <ul className="clean-list flex align-center">
-                <li onClick={() => onToggleMobileNav()}><NavLink to="/board">Board</NavLink></li>
-                <li onClick={() => onToggleMobileNav()}><NavLink to="/about">About</NavLink></li>
-            </ul>
-        </nav>
-    )
+class _BoardNav extends Component {
+
+    //     state = {
+    //         userImgStyle: {
+    //             backgroundImage: "url(https://res.cloudinary.com/ddgevj2yp/image/upload/v1610989052/avatar-1_kbr5un.jpg"
+    //         }
+    //     }
+
+    componentDidMount() {
+        if (!this.props.loggedInUser) return
+        const userImgUrl = this.props.loggedInUser.imgUrl
+        let userImgStyle = {
+            backgroundImage: `url(${userImgUrl})`
+        }
+        this.setState({ userImgStyle })
+    }
+
+
+
+    render() {
+        const { selectedBoard, toggleSideMenu } = this.props
+        return (
+            <div>
+                <nav>
+                    {/* <h5>{selectedBoard.title}</h5> */}
+                    title of board here
+                </nav>
+                <nav>
+                    {/* <span style={this.state.userImgStyle}></span> */}
+                    pics of board member here
+                </nav>
+                <nav>
+                    <button onClick={toggleSideMenu}> Activities Menu</button>
+                </nav>
+
+            </div>
+        )
+
+    }
 }
+
+
+const mapStateToProps = state => {
+    return {
+        // selectedBoard: state.boardModule.selectedBoard,
+        // loggedInUser: state.userModule.loggedInUser
+    }
+}
+const mapDispatchToProps = {
+
+}
+
+export const BoardNav = connect(mapStateToProps, mapDispatchToProps)(_BoardNav)
