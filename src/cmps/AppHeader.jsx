1d5a3a1dde238c920260e2639805fe2daa86ac43
Miriam Baranovska <maryam1649@gmail.com>
2021-05-28 15:18:11 +0300

    headers

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,17 +1,123 @@
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+// import { AddNewBoard } from './BoardHeader/AddNewBoard'
+// import logoFutura from '../assets/icons/newLogoFutura.png'
 
-export function AppHeader(props) {
-    return (
-        <header className="app-header flex justify-space-between">
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/board"> Boards </Link>
-            </nav>
-            <nav>app-logo</nav>
-            <nav>
-                {!props.loggedInUser && <Link to="/login"> Login</Link>}
-                {props.loggedInUser && <Link to="/login" style={{ backgroundImage: `url(${props.loggedInUser.imgUrl})` }}> </Link>}
-            </nav>
-        </header>
-    )
+import { connect } from 'react-redux'
+// import { LoginDrawer } from './LoginDrawer'
+import { onLogout } from '../store/actions/user-actions'
+// import { MemberPreview } from './BoardHeader/MemberPreview'
+// import { Dialog, IconButton } from '@material-ui/core'
+// import { Users } from './Users/UsersMain'
+
+import { userService } from '../services/user-service'
+// import { CloseOutlined } from '@material-ui/icons'
+
+
+export class _AppHeader extends Component {
+
+    state = {
+        isNewBoardModalShown: false,
+        isLoginDrawerShown: false,
+        isUserDetailsOpen: false,
+        loggedinUser: null
+    }
+
+
+    toggleModal = () => {
+        this.setState({ isNewBoardModalShown: !this.state.isNewBoardModalShown })
+    }
+
+    onCloseModal = () => {
+        this.setState({ isNewBoardModalShown: false })
+    } 
+
+    componentDidMount() {
+        this.setState({ loggedinUser: userService.getLoggedinUser() })
+    }
+
+    
+    redirectPath = (id) => {
+        this.props.history.replace(`/`)
+        this.props.history.replace(`/board/${id}`)
+        this.onCloseModal()
+    }
+
+    showLoginDrawer = (ev) => {
+        ev.stopPropagation()
+        ev.nativeEvent.stopImmediatePropagation();
+        this.setState({ isLoginDrawerShown: true })
+    }
+
+    hideLoginDrawer = (ev) => {
+        if (ev) ev.stopPropagation()
+        this.setState({ isLoginDrawerShown: false })
+    }
+
+    onOpenUserDetails = () => {
+        console.log('open')
+        this.setState({ isUserDetailsOpen: true })
+    }
+
+    onCloseUserDetails = () => {
+        this.setState({ isUserDetailsOpen: false })
+    }
+
+    onLogout = async (ev) => {
+        ev.stopPropagation()
+        await this.props.logout()
+        this.setState({ isUserDetailsOpen: false })
+    }
+
+    render() {        
+        return (
+            <React.Fragment className="flex">
+                {/* <Dialog open={this.state.isUserDetailsOpen} onClose={this.onCloseUserDetails} onBackdropClick={this.onCloseUserDetails}> */}
+                    {/* <div className="user-details-preview-header">
+                        <div>User Details</div>
+                        <button onClick={this.onCloseUserDetails}>
+                            <CloseOutlined />
+                        </button>
+                    </div> */}
+                    {/* <Users onCloseUserDetails={this.onCloseUserDetails} onLogout={this.onLogout}/> */}
+                {/* </Dialog> */}
+                {/* <Dialog open={this.state.isNewBoardModalShown} onClose={this.onCloseModal} onBackdropClick={this.onCloseModal}> */}
+                    {/* <div>Add new board</div> */}
+                    {/* <AddNewBoard onCloseModal={this.onCloseModal} redirectPath={this.redirectPath} /> */}
+                {/* </Dialog> */}
+                <div className="navbar-container flex">
+                    <div className="navbar-left-container flex">
+                        <div className="board-header-btn left"><NavLink to='/'><span className="material-icons">home </span></NavLink></div>
+                        <div className="board-header-btn left"><NavLink to='/board'><span className="material-icons"></span><h4 className="boards-text"> Boards </h4></NavLink></div>
+                    </div>
+
+                    {/* <div className="header-logo"><img src={logoFutura} alt="best logo ever" /></div> */}
+                    <div> logo here </div>
+
+                    <div className="navbar-right-container flex">
+                        <div className="board-header-btn right" onClick={this.toggleModal}><span className="material-icons">add</span></div>
+
+                        {(!this.props.loggedinUser) ? <div className="board-header-btn login right" onClick={this.showLoginDrawer}><h4 className="login-text">Login</h4></div> :''}
+                        {/* {(this.props.loggedinUser) ? <div onClick={this.onOpenUserDetails} className="member-preview-header-container"><MemberPreview name={this.props.loggedinUser.fullName} imgUrl={this.props.loggedinUser.imgUrl} /></div> : <React.Fragment />} */}
+{/* 
+                        <LoginDrawer isShowing={this.state.isLoginDrawerShown} hideLoginDrawer={this.hideLoginDrawer} /> */}
+                        <div> Login Here</div>
+
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
 }
+
+const mapStateToProps = state => {
+    return {
+        // loggedinUser: state.userReducer.loggedinUser
+    }
+}
+
+const mapDispatchToProps = {
+    onLogout
+}
+
+export const AppHeader = connect(mapStateToProps, mapDispatchToProps)(_AppHeader);
--- src/cmps/BoardNav.jsx
+++ src/cmps/BoardNav.jsx
@@ -1,57 +1,55 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+// import { MemberList } from './MemberList';
+// import { Filter } from './Filter';
+// import { loadAllUsers } from '../store/actions/user-actions';
+// import { Notifications } from './Notifications';
+// import MoreHorizOutlinedIcon from '@material-ui/icons/MoreHorizOutlined';
 
 
-class _BoardNav extends Component {
 
-    //     state = {
-    //         userImgStyle: {
-    //             backgroundImage: "url(https://res.cloudinary.com/ddgevj2yp/image/upload/v1610989052/avatar-1_kbr5un.jpg"
-    //         }
-    //     }
+export class _BoardNav extends Component {
 
-    componentDidMount() {
-        if (!this.props.loggedInUser) return
-        const userImgUrl = this.props.loggedInUser.imgUrl
-        let userImgStyle = {
-            backgroundImage: `url(${userImgUrl})`
-        }
-        this.setState({ userImgStyle })
+    async componentDidMount() {
+        // await this.props.loadAllUsers()
     }
-
-
-
+    
     render() {
-        const { selectedBoard, toggleSideMenu } = this.props
+        
         return (
-            <div>
-                <nav>
-                    {/* <h5>{selectedBoard.title}</h5> */}
-                    title of board here
-                </nav>
-                <nav>
-                    {/* <span style={this.state.userImgStyle}></span> */}
-                    pics of board member here
-                </nav>
-                <nav>
-                    <button onClick={toggleSideMenu}> Activities Menu</button>
-                </nav>
+                <div className="boards-header-container flex " >
+                    {/* <h3>{this.props.title}</h3> */}
+                    Board Title Here
+                    <div className="members-container">
+                        {/* <MemberList members={this.props.members} allUsers={this.props.allUsers}/> */}
+                        Member List Here
+                    </div>
 
-            </div>
+
+                    {/* <Filter onFilter={this.props.onFilter} /> */}
+                    Filter here
+                    
+                    {/* <Notifications board={this.props.board} lastUpdate={this.props.lastUpdate}/> */}
+                    Notifications Here
+                    <div className="board-header-btn show-menu" onClick={() => this.props.onToggleSidebar(true)}>
+                        {/* <MoreHorizOutlinedIcon /> */}
+                        <p>Activities Menu Here</p>
+                    </div>
+                </div>
         )
-
     }
 }
 
 
 const mapStateToProps = state => {
     return {
-        // selectedBoard: state.boardModule.selectedBoard,
-        // loggedInUser: state.userModule.loggedInUser
-    }
-}
+        board: state.boardModule.board,
+        allUsers: state.userModule.users
+    };
+};
+
 const mapDispatchToProps = {
+    // loadAllUsers
+};
 
-}
-
-export const BoardNav = connect(mapStateToProps, mapDispatchToProps)(_BoardNav)
+export const BoardNav = connect(mapStateToProps, mapDispatchToProps)(_BoardNav);
