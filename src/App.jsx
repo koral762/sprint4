c0b0ab0270044c3b5ce29aef4a6f855d504509b3
KsenseiB <ksu0593@gmail.com>
2021-05-31 21:48:44 +0300

    kenia mon night half working dnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import { routes } from './routes'
 import { connect } from 'react-redux'
+import { DragDropContext } from 'react-beautiful-dnd'
 import { AppHeader } from './cmps/AppHeader'
-import React, { Component } from 'react';
 import { onLogout } from './store/actions/user-actions.js'
+import { boardService } from './services/board-service.js'
 // import { createMuiTheme, ThemeProvider, } from '@material-ui/core'
 
-// const theme = createMuiTheme({
-//   palette: {
-//     primary: {
-//       main: '#a9d6e5',
-//     },
-//     secondary: {
-//       main: '#ffff',
-//     }
 
-//   }
-
-// })
 
 class _App extends Component {
+
+  onDragStart = () => {
+    this.props.resetFilterBy(this.props.board._id)
+  }
+
+  onDragEnd = (result) => {}
+
   render() {
     const { onLogout, loggedinUser } = this.props
     return (
       // <ThemeProvider theme={theme}>
-      <div className="main-container flex column full" >
-        <AppHeader onLogout={onLogout} loggedinUser={loggedinUser} />
-        <Switch className="content">
-          {routes.map(route => <Route key={route.path} exact component={route.component} path={route.path} />)}
-        </Switch>
-      </div>
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        <div className="main-container flex column full" >
+          <AppHeader onLogout={onLogout} loggedinUser={loggedinUser} />
+          <Switch className="content">
+            {routes.map(route => <Route key={route.path} exact component={route.component} path={route.path} />)}
+          </Switch>
+        </div>
+      </DragDropContext>
       /* </ThemeProvider> */
     )
   }
--- src/cmps/groups/GroupPreview.jsx
+++ src/cmps/groups/GroupPreview.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Droppable, Draggable } from 'react-beautiful-dnd'
 import { CardPreview } from '../cards/CardPreview'
-import { addCard, setNewGroupName} from '../../store/actions/board-actions.js'
+import { addCard, setNewGroupName } from '../../store/actions/board-actions.js'
 import { NewItem } from '../NewItem'
 import { GroupMenu } from '../groups/GroupMenu'
 import { ClickAwayListener } from '@material-ui/core';
 
 class _GroupPreview extends Component {
-
     state = {
         currGroupName: '',
         currGroupId: null,
@@ -32,14 +32,13 @@ class _GroupPreview extends Component {
         ev.preventDefault()
         const { currGroupId, currGroupName } = this.state
         this.props.setNewGroupName(currGroupId, currGroupName, this.props.board)
-        this.setState({ isChangeGroupShown: false})
+        this.setState({ isChangeGroupShown: false })
     }
 
     toggleMenu = (ev = null) => {
         if (ev) ev.stopPropagation()
         const isShown = !this.state.isMenuShown
         this.setState({ isMenuShown: isShown })
-
     }
 
     onKeyPress = (ev) => {
@@ -47,54 +46,81 @@ class _GroupPreview extends Component {
             this.closeChangeGroupName(ev)
         }
     }
+
     openHeadrEdit = (ev = null) => {
         if (ev) ev.stopPropagation()
         this.setState({ isChangeGroupShown: true })
     }
+
     closeChangeGroupName = (ev) => {
         this.setState({ isChangeGroupShown: false })
         this.onSubmit(ev)
     }
+
     onOpenChangeGroupName = (id, groupName) => {
         this.setState({ currGroupId: id, currGroupName: groupName, isChangeGroupShown: true })
     }
     render() {
-
         const group = this.props.group
 
         return (
+            <Draggable draggableId={group.id} index={this.props.index}>
+                {(provided) => (
+                    <section
+                        className="card-list group-container"
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}>
+                        <div className="group-preview-header" {...provided.dragHandleProps} >
+                            {!this.state.isChangeGroupShown
+                                && <div onClick={() =>
+                                    this.onOpenChangeGroupName(group.id, group.title)}>{group.title}</div>}
+                            {(this.state.isChangeGroupShown) ?
+                                <ClickAwayListener onClickAway={this.closeChangeGroupName}>
+                                    <form onSubmit={this.onSubmit} className="change-group-name">
+                                        <input className="change-group-name-input"
+                                            type="text"
+                                            name="group-name"
+                                            onKeyPress={this.onKeyPress}
+                                            onChange={this.handleChangeGroupName}
+                                            defaultValue={group.title}
+                                            autoFocus spellCheck="false" autoComplete="off"
+                                            onFocus={ev => ev.target.select()} />
+                                    </form>
+                                </ClickAwayListener> : ''}
+                            <span onClick={this.toggleMenu} className="list-header-extras material-icons dots-icon">more_horiz</span>
+                            {this.state.isMenuShown
+                                && <GroupMenu
+                                    toggleMenu={this.toggleMenu}
+                                    groupId={group.id}
+                                    onAdd={this.onAddCard} />}
+                        </div>
+                        <div className="cards-content-wrapper"> {
+                            group.cards.map((card) => <CardPreview key={card.id}
+                                card={card}
+                                history={this.props.history}
+                            />)}
+                        </div >
+                        <Droppable droppableId={group.id} type="card">
+                            {(provided) => (
+                                <div className="card-container"
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}>
+                                    {group.cards.map((card, index) => {
+                                        if (!card.archivedAt) {
+                                            return <CardPreview key={card.id} card={card} index={index} />
+                                        }
+                                    })}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                        <div className="new-card-btn-container">
+                            <NewItem addItemTxt={this.getAddItemTxt()} placeHolderTxt='Add a card title..' addBtnTxt="Add Card" onAdd={this.onAddCard} />
+                        </div>
+                    </section>
+                )}
+                </Draggable>
 
-            <section className="card-list group-container">
-                <div className="group-preview-header">
-                    {!this.state.isChangeGroupShown && <div onClick={() => this.onOpenChangeGroupName(group.id, group.title)}>{group.title}</div>}
-                    {(this.state.isChangeGroupShown) ?
-                        <ClickAwayListener onClickAway={this.closeChangeGroupName}>
-                            <form onSubmit={this.onSubmit} className="change-group-name">
-                                <input className="change-group-name-input"
-                                    type="text" name="group-name" onKeyPress={this.onKeyPress} onChange={this.handleChangeGroupName}
-                                    defaultValue={group.title}
-                                    autoFocus spellCheck="false" autoComplete="off"
-                                    onFocus={ev => ev.target.select()} />
-                            </form>
-                        </ClickAwayListener> : ''}
-                    <span onClick={this.toggleMenu} className="list-header-extras material-icons dots-icon">more_horiz</span>
-                    {
-                        this.state.isMenuShown && <GroupMenu toggleMenu={this.toggleMenu} groupId={group.id}
-                            onAdd={this.onAddCard} />
-                    }
-                </div>
-
-                <div className="cards-content-wrapper"> {
-                    group.cards.map((card) => <CardPreview key={card.id}
-                        card={card}
-                        history={this.props.history}
-                    />)
-                }
-                </div >
-                <div className="new-card-btn-container">
-                    <NewItem addItemTxt={this.getAddItemTxt()} placeHolderTxt='Add a card title..' addBtnTxt="Add Card" onAdd={this.onAddCard} />
-                </div>
-            </section >
         )
     }
 }
